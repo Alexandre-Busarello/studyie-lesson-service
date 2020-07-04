5cbbe0d22ea5c5dac66539842e25acf5c2fb14ac
Busa <alexandre.busarello@descomplica.com.br>
2020-07-03 22:46:54 -0300

    Change content types and point

--- src/domain/content/routes.ts
+++ src/domain/content/routes.ts
@@ -3,11 +3,11 @@ import { ContentTypeModel } from '@app/domain/content/content-type';
 
 const routes = Router();
 
-routes.get('/content', async (req: any, res: any) => {
+routes.get('/content/types', async (req: any, res: any) => {
   return res.json(await ContentTypeModel.getAll());
 });
 
-routes.post('/content', async (req: any, res: any) => {
+routes.post('/content/types', async (req: any, res: any) => {
   return res.json(await ContentTypeModel.create(req.body));
 });
 
