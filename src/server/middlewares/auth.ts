6b5cfd076f3738ce2666845eba95f0a4f5e24234
Busa <alexandre.busarello@descomplica.com.br>
2020-07-05 20:16:28 -0300

    Remove logs

--- src/server/middlewares/auth.ts
+++ src/server/middlewares/auth.ts
@@ -6,29 +6,18 @@ import authConfig from '@app/config/auth';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log('header');
-  console.log(authHeader);
-
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provided' });
   }
 
-  console.log(1);
-
   const [, token] = authHeader.split(' ');
 
-  console.log(2);
-
   try {
-    console.log(3);
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    console.log(4);
-    console.log(decoded);
     req.userId = decoded.userId;
 
     return next();
   } catch (err) {
-    console.log(err);
     return res.status(401).json({ error: 'Token invalid' });
   }
 };
